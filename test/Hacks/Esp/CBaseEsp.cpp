7d8fef050db854793c90d9a00bd34bca6bf2ed0d
Vladislav Alpatov <alpatof74@gmail.com>
2022-02-14 23:39:46 +0300

    changed w2s function

--- test/Hacks/Esp/CBaseEsp.cpp
+++ test/Hacks/Esp/CBaseEsp.cpp
@@ -3,20 +3,27 @@
 
 ImVec3 CBaseEsp::WorldToScreen(const ImVec3& vecPosition)
 {
+    // Screen size 
     ImVec2 viewPorstSize = ImGui::GetMainViewport()->Size;
 
     auto client = GlobalVars::client;
 
     float _x = client->dwViewmatrix[0][0] * vecPosition.x + client->dwViewmatrix[0][1] * vecPosition.y + client->dwViewmatrix[0][2] * vecPosition.z + client->dwViewmatrix[0][3];
     float _y = client->dwViewmatrix[1][0] * vecPosition.x + client->dwViewmatrix[1][1] * vecPosition.y + client->dwViewmatrix[1][2] * vecPosition.z + client->dwViewmatrix[1][3];
-    float _z = client->dwViewmatrix[2][0] * vecPosition.x + client->dwViewmatrix[2][1] * vecPosition.y + client->dwViewmatrix[2][2] * vecPosition.z + client->dwViewmatrix[2][3];
+    // Dont need z cuz we need only x,y and w
+    //float _z = client->dwViewmatrix[2][0] * vecPosition.x + client->dwViewmatrix[2][1] * vecPosition.y + client->dwViewmatrix[2][2] * vecPosition.z + client->dwViewmatrix[2][3];
+
+    // w is depth
     float w =  client->dwViewmatrix[3][0] * vecPosition.x + client->dwViewmatrix[3][1] * vecPosition.y + client->dwViewmatrix[3][2] * vecPosition.z + client->dwViewmatrix[3][3];
 
-    POLY_MARKER;
+    ImVec2 ndc;
 
-    float x = (viewPorstSize.x / 2.f * (_x / w)) + ((_x / w) + viewPorstSize.x / 2.f);
+    ndc.x = _x / w;
+    ndc.y = _y / w;
 
-    float y = -(viewPorstSize.y / 2.f * (_y / w)) + ((_y / w) + viewPorstSize.y / 2.f);
+    // Normalize screen cords
+    float x =  (viewPorstSize.x / 2.f * ndc.x) + (ndc.x + viewPorstSize.x / 2.f);
+    float y = -(viewPorstSize.y / 2.f * ndc.y) + (ndc.y + viewPorstSize.y / 2.f);
 
     return ImVec3(x, y, w);
 }

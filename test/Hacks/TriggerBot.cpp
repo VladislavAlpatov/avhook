4f0be27d818ed3de6bd4a6a2b8c4f0b96bfdddf6
Владислав Алпатов <alpatof74@gmail.com>
2022-10-06 08:49:17 +0300

    small changes

--- test/Hacks/TriggerBot.cpp
+++ test/Hacks/TriggerBot.cpp
@@ -28,17 +28,23 @@ bool TriggerBot::IsEnemyEntityInSight()
 {
 	const auto pLocalPlayer = SSDK::ClientBase::GetLocalPlayer();
 	const auto vecRayStart = pLocalPlayer->m_vecOrigin + pLocalPlayer->m_vecViewOffset;
+
 	POLY_MARKER;
+
 	SSDK::CGameTrace   trace;
 	SSDK::Ray_t        ray;
 	SSDK::CTraceFilter tracefilter;
 	tracefilter.m_pSkip = pLocalPlayer;
+
 	POLY_MARKER;
 	ray.Init(vecRayStart, vecRayStart.Transform(pLocalPlayer->m_vecViewAngles + pLocalPlayer->m_vecAimPuchAngle, 9000.f));
 	GlobalVars::g_pIEngineTrace->TraceRay(ray, MASK_SHOT | CONTENTS_GRATE, &tracefilter, &trace);
 
+
 	const auto pHitEntity = (SSDK::CBaseEntity*)trace.m_pHitEntity;
 	POLY_MARKER;
+
+
 	return !(!pHitEntity or (pHitEntity->GetClientClass()->m_iClassId != SSDK::ClassIndex::CCSPlayer) or !pHitEntity->IsAlive() or pHitEntity->m_iTeamNum == pLocalPlayer->m_iTeamNum);
 
 }
